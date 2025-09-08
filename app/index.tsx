3033556a3a74aa0281d8f1e862323523535014c5
houssam chakir <thisischchannel@gmail.com>
2025-09-08 21:20:47 +0100

    Update app configuration and integrate Tailwind CSS with NativeWind
    - Changed app scheme from "rnativemovieapp" to "movies" in app.json.
    - Added Babel and Metro configuration files for NativeWind integration.
    - Introduced Tailwind CSS configuration and global styles.
    - Updated package.json and package-lock.json to include NativeWind and Tailwind CSS dependencies.
    - Removed unused components and files to streamline the project structure.
    - Simplified the layout and index files for better readability and performance.

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className='flex-1 justify-center items-center bg-primary'>
+      <Text className='text-accent text-xl font-bold'>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
